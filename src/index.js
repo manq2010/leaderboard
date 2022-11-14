0da22e03ac629c5eeb47857eeb0b5771cebbcdde
manq2010 <manq2010@gmail.com>
2022-11-14 21:39:37 +0200

    Separate code into more ES6 modules

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import Leader from './modules/player.js';
 import display from './modules/display.js';
 import clearScoresInputs from './modules/clear-inputs.js';
 import addPlayer from './modules/add-player.js';
+import renderNavigation from './modules/nav.js';
+import renderHome from './modules/home.js';
+import renderContact from './modules/contact.js';
+import renderFooter from './modules/footer.js';
 
 const addBtn = document.querySelector('form');
 const addName = document.getElementById('add-name');
@@ -11,6 +15,42 @@ const addScore = document.getElementById('add-score');
 const regex = /^[0-9]+$/;
 const refreshBtn = document.querySelector('.refresh-btn');
 const inputMsg = document.querySelector('.input-msg');
+
+renderNavigation();
+renderHome();
+renderFooter();
+
+const navLinks = document.querySelectorAll('.nav-link');
+const mainContainer = document.querySelector('.m-container');
+
+const renderPage = (location, page) => {
+  location.innerHTML = '';
+  page();
+};
+
+const removeActive = () => {
+  navLinks.forEach((link) => {
+    if (link.classList.contains('active')) {
+      link.classList.remove('active');
+    }
+  });
+};
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    if (e.target.textContent === 'Home') {
+      removeActive();
+      e.target.classList.add('active');
+      renderPage(mainContainer, renderHome);
+    }
+
+    if (e.target.textContent === 'Contact') {
+      removeActive();
+      e.target.classList.add('active');
+      renderPage(mainContainer, renderContact);
+    }
+  });
+});
 
 addBtn.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -38,7 +78,3 @@ addBtn.addEventListener('submit', (e) => {
 refreshBtn.addEventListener('click', () => {
   display();
 });
-
-const footerDate = document.querySelector('.footer-date');
-const date = new Date();
-footerDate.innerHTML = date.getFullYear();
